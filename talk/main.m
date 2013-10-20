5c3c4e50cfc9f72a9a0b93d8c7cece9bcd88d4a9
yr1216 <wangshuai@wangshuais-MacBook-Pro.local>
2013-10-20 19:44:27 +0100

    Initial Commit

---
+++ talk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  talk
+//
+//  Created by wangshuai on 13-10-20.
+//  Copyright (c) 2013年 wangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ talkTests/talkTests.m
@@ -0,0 +1,34 @@
+//
+//  talkTests.m
+//  talkTests
+//
+//  Created by wangshuai on 13-10-20.
+//  Copyright (c) 2013年 wangshuai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface talkTests : XCTestCase
+
+@end
+
+@implementation talkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
